55bfc555d9f629286ee2b8640463fa201241d193
Clairaxe <128609005+Clairaxe@users.noreply.github.com>
2023-03-22 14:52:50 +0100

    Update accueil.php

--- accueil.php
+++ accueil.php
@@ -5,9 +5,6 @@
     <meta charset="utf-8">
     <title>JDR</title>
     <link rel = "stylesheet" href = "style.css">
-  </head>
-
-  <head>
 	<ul class="menu">
    	  <li>
             <a href="http://pams.script.univ-paris-diderot.fr/~chambazc/Projet/accueil.php"> <img src="https://club-jdr.fr/wp-content/uploads/2021/12/jdr.png" width=50 height=50 > </a>
