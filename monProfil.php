04d441748bb751d0c17db21f0cffa794020c4a61
Clairaxe <128609005+Clairaxe@users.noreply.github.com>
2023-03-23 12:15:51 +0100

    Add files via upload

---
+++ monProfil.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+  <head>
+    <meta charset="utf-8">
+    <title>JDR</title>
+  </head>
+
+    <body>
+      <div class="profile2">
+        <div class="profile2-titre">
+          <h2 class="infos-perso"> Mes informations personnelles:</h2><br>
+        </div>
+        <div class="profile2-info">
+            <p class="firstname"> <b>Prénom:</b> Claire</p>
+            <p class="lastname"> <b>Nom:</b> Chambaz </p>
+            <p class="email"> <b>e-mail:</b> claire.chambaz@gmail.com </p>
+            <p class="phone"> <b>numéro de téléphone:</b> 0769598061 </p>
+        </div>
+        <div class="mot2passe">
+          <input type="button" value="changer mon mot de passe">
+        </div>
+      </div>
+    </body>
+
+</html>
